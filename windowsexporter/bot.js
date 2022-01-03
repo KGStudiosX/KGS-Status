94783d860f198dc4e0aa3c07f2f5fedf67031fbc
Kycb <kycb42148@gmail.com>
2022-01-04 00:55:25 +0300

    Rewrite most of code (Windows exporter)

--- windowsexporter/bot.js
+++ windowsexporter/bot.js
@@ -1,29 +1,16 @@
 const Discord = require(`discord.js`);
-const fs = require('fs');
-const http = require('http');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const data = require('./data.json');
 
-let data;
-try {
-    data = JSON.parse(fs.readFileSync(`data.json`));
-    if(data.token == null || data.token == "") {
-        if(data.nodee_address == null || data.nodee_address == "") console.error("Please add token and Windows Exporter url to data.json"); else console.error("Please add token to data.json");
-        process.exit(1);
-    }
-    if(data.nodee_address == null || data.nodee_address == ""){
-        console.error("Please add Windows Exporter url to data.json");
-        process.exit(1);
-    }
-}
-catch(e){
-    data = { token: "", nodee_address: "" };
-    fs.writeFileSync(`data.json`, JSON.stringify(data));
-    console.error("Please add token to data.json");
+if(!data.nodee_address || !data.token){
+    console.error("lease add token and Node Exporter url to data.json");
     process.exit(1);
 }
 
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 client.login(data.token).then(() => {
     console.log("Bot launched. (WindowsExporter)");
+    updateStatusRAM();
     setInterval(() => {
         updateStatusRAM();
         setTimeout(() => {
@@ -46,85 +33,55 @@ client.on('messageCreate', message => {
 
 let nodee;
 const updateStatusCPU = async() => {
-    const request = http.get(data.nodee_address, (response) => {
-        if (response.statusCode != 200) {
-            client.user.setPresence({ activities: [{ name: 'offline' }], status: 'idle' });
-            return;
+    const res = await fetch(data.nodee_address);
+    const nodedata = await res.text();
+
+    nodee = nodedata.split('\n');
+    let cpuUsage, cpuTotal, cpuUsage1 = 0, cpuTotal1 = 0, cpuUsage2 = 0, cpuTotal2 = 0; // CPU
+    const cpuRegex = /^windows_cpu_time_total{core="(0,\d)",mode="(dpc|idle|interrupt|privileged|user)"} (\d+([.]\d*)?(e[+-]?\d+)?)/i;
+    nodee.forEach(element => {
+        if(element.match(cpuRegex)){
+            if(element.match(cpuRegex)[2] != "idle")cpuUsage1 += parseFloat(element.match(cpuRegex)[3]);
+            cpuTotal1 += parseFloat(element.match(cpuRegex)[3]);
         }
-        let rawdata = '';
-        response.setEncoding('utf8');
-        response.on('data', (chunk) => rawdata += chunk);
-        response.on('end', () => {
-            try{
-                nodee = rawdata.split('\n');
-                let cpuUsage, cpuTotal, cpuUsage1 = 0, cpuTotal1 = 0, cpuUsage2 = 0, cpuTotal2 = 0; // CPU
-                const cpuRegex = /^windows_cpu_time_total{core="(0,\d)",mode="(dpc|idle|interrupt|privileged|user)"} (\d+([.]\d*)?(e[+-]?\d+)?)/i;
-                nodee.forEach(element => {
-                    if(element.match(cpuRegex)){
-                        if(element.match(cpuRegex)[2] != "idle")cpuUsage1 += parseFloat(element.match(cpuRegex)[3]);
-                        cpuTotal1 += parseFloat(element.match(cpuRegex)[3]);
-                    }
-                });
+    });
 
-                setTimeout(() => {
-                    const request2 = http.get(data.nodee_address, (response2) => {
-                        let rawdata2 = '';
-                        response2.setEncoding('utf8');
-                        response2.on('data', (chunk2) => rawdata2 += chunk2);
-                        response2.on('end', () => {
-                            let nodee2 = rawdata2.split('\n');
-                            nodee2.forEach(element => {
-                                if(element.match(cpuRegex)){
-                                    if(element.match(cpuRegex)[2] != "idle")cpuUsage2 += parseFloat(element.match(cpuRegex)[3]);
-                                    cpuTotal2 += parseFloat(element.match(cpuRegex)[3]);
-                                }
-                            });
-                            cpuUsage = cpuUsage2 - cpuUsage1;
-                            cpuTotal = cpuTotal2 - cpuTotal1;
+    setTimeout(async() => {
+        const res2 = await fetch(data.nodee_address);
+        const nodedata2 = await res2.text();
 
-                            const cpuPercent = (cpuUsage / cpuTotal) * 100;
-
-                            client.user.setPresence({activities: [{name: `CPU: ${Math.round(cpuPercent)}%`, type: "WATCHING"}], status: 'online'});
-                        });
-                    });
-                }, 1000);
-            } catch (e){
-                console.log(e);
+        let nodee2 = nodedata2.split('\n');
+        nodee2.forEach(element => {
+            if(element.match(cpuRegex)){
+                if(element.match(cpuRegex)[2] != "idle")cpuUsage2 += parseFloat(element.match(cpuRegex)[3]);
+                cpuTotal2 += parseFloat(element.match(cpuRegex)[3]);
             }
         });
-    });
-    request.on('error', () => client.user.setPresence({ activities: [{ name: 'offline' }], status: 'idle' }));
+        cpuUsage = cpuUsage2 - cpuUsage1;
+        cpuTotal = cpuTotal2 - cpuTotal1;
+
+        const cpuPercent = (cpuUsage / cpuTotal) * 100;
+
+        client.user.setPresence({activities: [{name: `CPU: ${Math.round(cpuPercent)}%`, type: "WATCHING"}], status: 'online'});
+    }, 1000);
 }
 
 const updateStatusRAM = async() => {
-    const request = http.get(data.nodee_address, (response) => {
-        if (response.statusCode != 200) {
-            client.user.setPresence({ activities: [{ name: 'offline' }], status: 'idle' });
-            return;
-        }
-        let rawdata = '';
-        response.setEncoding('utf8');
-        response.on('data', (chunk) => rawdata += chunk);
-        response.on('end', () => {
-            try{
-                nodee = rawdata.split('\n');
-                let memTotal, memFree; // RAM
-                nodee.forEach(element => {
-                    if(element.startsWith("windows_os_virtual_memory_bytes")) memTotal = +(element.replaceAll("windows_os_virtual_memory_bytes ", "")); else
-                    if(element.startsWith("windows_os_physical_memory_free_bytes" )) memFree  = +(element.replaceAll("windows_os_physical_memory_free_bytes " , ""));
-                });
+    const res = await fetch(data.nodee_address);
+    const nodedata = await res.text();
 
-                const memoryTotal = memTotal;
-                const memoryFree = memFree;
-                const memoryUsed = memoryTotal - memoryFree;
-                const memoryPercent = Math.floor((memoryUsed / memoryTotal) * 100);
-
-                const memoryString = `${memoryPercent}% (${((memoryUsed / 1048576) / 1024).toFixed(2)}GB/${((memoryTotal / 1048576) / 1024).toFixed(2)}GB)`;
-                client.user.setPresence({activities: [{name: `RAM: ${memoryString}`, type: "WATCHING"}], status: 'online'});
-            } catch (e){
-                console.log(e);
-            }
-        }); 
+    nodee = nodedata.split('\n');
+    let memTotal, memFree; // RAM
+    nodee.forEach(element => {
+        if(element.startsWith("windows_os_virtual_memory_bytes")) memTotal = +(element.replaceAll("windows_os_virtual_memory_bytes ", "")); else
+        if(element.startsWith("windows_os_physical_memory_free_bytes" )) memFree  = +(element.replaceAll("windows_os_physical_memory_free_bytes " , ""));
     });
-    request.on('error', () => client.user.setPresence({ activities: [{ name: 'offline' }], status: 'idle' }));
+
+    const memoryTotal = memTotal;
+    const memoryFree = memFree;
+    const memoryUsed = memoryTotal - memoryFree;
+    const memoryPercent = Math.floor((memoryUsed / memoryTotal) * 100);
+
+    const memoryString = `${memoryPercent}% (${((memoryUsed / 1048576) / 1024).toFixed(2)}GB/${((memoryTotal / 1048576) / 1024).toFixed(2)}GB)`;
+    client.user.setPresence({activities: [{name: `RAM: ${memoryString}`, type: "WATCHING"}], status: 'online'});
 }
